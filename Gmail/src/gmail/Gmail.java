1f07b6c7b3bbcc9dfd045b87665d3af2d5dcd73f
kevin001rosas <kevin001rosas@gmail.com>
2016-05-11 01:33:19 -0500

    Proyecto inicial

---
+++ Gmail/src/gmail/Gmail.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gmail;
+
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ *
+ * @author lelguea
+ */
+public class Gmail {
+public static void main(String[] args) {
+    
+        System.setProperty("java.net.preferIPv6Stack" , "true");
+
+		final String username = "lelguea@up.edu.mx";
+		
+                // TLS
+                /*
+		Properties props = new Properties();
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.starttls.enable", "true");
+		props.put("mail.smtp.host", "2607:f8b0:4003:c14::6c");
+		props.put("mail.smtp.port", "587");
+                */
+                
+                Properties props = new Properties();
+		props.put("mail.smtp.host", "2607:f8b0:4003:c14::6c");
+		props.put("mail.smtp.socketFactory.port", "465");
+		props.put("mail.smtp.socketFactory.class",
+				"javax.net.ssl.SSLSocketFactory");
+		props.put("mail.smtp.auth", "true");
+		props.put("mail.smtp.port", "465");
+
+		Session session = Session.getInstance(props,
+		  new javax.mail.Authenticator() {
+			protected PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(username, password);
+			}
+		  });
+
+		try {
+
+			Message message = new MimeMessage(session);
+			message.setFrom(new InternetAddress(username));
+			message.setRecipients(Message.RecipientType.TO,
+				InternetAddress.parse("lor@up.mx"));
+			message.setSubject("Prueba 3 Texto Plano SSL");
+			message.setText("Dear Lor, Have a nice Day");
+                        message.setContent("<h1>hola</h1>, <b>saludos</b><br>Bye", "text/html; charset=utf-8");
+
+			Transport.send(message);
+
+			System.out.println("Done");
+
+		} catch (MessagingException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
